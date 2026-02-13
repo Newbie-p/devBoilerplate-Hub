62c0b046154df196b8a766faec0df895e984f1c9
Newbie-p <prafulsuryawanshi760@gmail.com>
2026-02-13 16:09:32 +0530

    Initial backend setup: Express server, MongoDB Atlas connection, project structure

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from 'cors';
+import connectDB from "./config/mongo.config.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    res.json({message : "Dev Boilerplate Hub API is running"});
+})
+
+app.listen(process.env.PORT, ()=>{
+    connectDB();
+    console.log(`server is running on port ${process.env.PORT}`);
+})
